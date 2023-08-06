bf4ce06e0875a2fb55c869617a281a92b608d2a1
Pavel Gri <zlibabaika@gmail.com>
2023-08-07 02:03:42 +0400

    Initial Commit

---
+++ TeamBoom/TeamBoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamBoomApp.swift
+//  TeamBoom
+//
+//  Created by Павел Грицков on 07.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TeamBoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
