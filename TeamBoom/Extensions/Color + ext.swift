a3c10ab04435d5b3b1b302ccea060db848f91032
Artem <moiseev@physics.msu.ru>
2023-08-07 14:20:23 +0300

    add Linter to the project

--- TeamBoom/Extensions/Color + ext.swift
+++ TeamBoom/Extensions/Color + ext.swift
@@ -15,11 +15,11 @@ extension ShapeStyle where Self == Color {
     static var yellowOfButton: Color {
         Color(red: 0.98, green: 1.0, blue: 0)
     }
-    
+
     static var yellowGradient: Color {
         Color(red: 0.96, green: 0.97, blue: 0.04)
     }
-    
+
     static var orangeGradient: Color {
         Color(red: 1.0, green: 0.6, blue: 0.019).opacity(0.63)
     }
