40555e23f456eedf884f694f80b24ebb7f17dd16
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-08-07 19:47:04 +0300

    add layout for mainVC

---
+++ TeamBoom/MainViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MainViewController.swift
+//  TeamBoom
+//
+//  Created by Daniil Kulikovskiy on 07.08.2023.
+//
+
+import SwiftUI
+
+struct MainViewController: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                BackgroundGradientView()
+                VStack {
+                    Image("textBomb")
+                    Image("bomb")
+                    VStack(spacing: 15) {
+                        Button() {
+                            
+                        } label: {
+                            Text("Старт")
+                                .font(.system(size: 22, weight: .bold))
+                        }
+                        .frame(width: 274, height: 79)
+                        .background(.violet)
+                        .foregroundColor(.yellowGradient)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+                        
+                        Button() {
+                          
+                        } label: {
+                            Text("Категории")
+                                .font(.system(size: 22, weight: .bold))
+                        }
+                        .frame(width: 274, height: 79)
+                        .background(.violet)
+                        .foregroundColor(.yellowGradient)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+                        
+                        HStack {
+                            Spacer()
+                            Image("question-mark")
+                                .padding(.trailing, 30)
+                        }
+//
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("Игра для компании", displayMode: .inline)
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+struct MainViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MainViewController()
+    }
+}
