7aa72a7c6c41124899ce163db2acea48d714ce6d
Иван Лясковец <lyaskovets.iv@MacBook-Air-Ivan.local>
2023-08-08 14:28:44 +0300

    A lot of refactor actions

--- TeamBoom/Extensions/Color + ext.swift
+++ TeamBoom/Extensions/Color + ext.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 extension ShapeStyle where Self == Color {
-    static var violet: Color {
-        Color(red: 0.5, green: 0.18, blue: 0.56)
-    }
+	static var violet: Color {
+		Color(red: 0.5, green: 0.18, blue: 0.56)
+	}
 
-    static var yellowOfButton: Color {
-        Color(red: 0.98, green: 1.0, blue: 0)
-    }
+	static var yellowOfButton: Color {
+		Color(red: 0.98, green: 1.0, blue: 0)
+	}
 
-    static var yellowGradient: Color {
-        Color(red: 0.96, green: 0.97, blue: 0.04)
-    }
+	static var yellowGradient: Color {
+		Color(red: 0.96, green: 0.97, blue: 0.04)
+	}
 
-    static var orangeGradient: Color {
-        Color(red: 1.0, green: 0.6, blue: 0.019).opacity(0.63)
-    }
+	static var orangeGradient: Color {
+		Color(red: 1.0, green: 0.6, blue: 0.019).opacity(0.63)
+	}
 }
