36b259563e6480857c10a90414433cefd13d8510
Pavel Gri <zlibabaika@gmail.com>
2023-08-12 16:18:48 +0400

    refactoring and fix warnings

--- TeamBoom/Screens/GameView/GameEndView.swift
+++ TeamBoom/Screens/GameView/GameEndView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct GameEndView: View {
+    private let timeIsOverTitle = "Время вышло!"
+    private let taslTitle = "Проигравший выполняет задание"
+    
+    private var isPunishment: Bool {
+        gameModel.withPunishment
+    }
+    
 	// MARK: - States&Properties
 
 	@ObservedObject var gameModel: GameViewModel
@@ -15,29 +22,22 @@ struct GameEndView: View {
 	// MARK: - UI
 
 	var body: some View {
-		ZStack {
-			BackgroundGradientView()
 			VStack {
-				if gameModel.withPunishment {
-					Text("Проигравший выполняет задание")
-						.padding()
-						.multilineTextAlignment(.center)
-						.font(.custom(CustomFonts.DelaGothicOne, size: 24))
-				} else {
-					Text("Время вышло!")
-						.padding()
-						.multilineTextAlignment(.center)
-						.font(.custom(CustomFonts.DelaGothicOne, size: 24))
-				}
+                Text(isPunishment ? taslTitle : timeIsOverTitle)
+                    .padding()
+                    .font(.custom(CustomFonts.DelaGothicOne, size: 24))
+                
 				Image("explosion")
-				if gameModel.withPunishment {
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+				if isPunishment {
 					Text(gameModel.punishment)
-                        .padding(8)
+                        .padding()
 						.foregroundColor(.violet)
 						.font(.custom(CustomFonts.DelaGothicOne, size: 20))
-						.multilineTextAlignment(.center)
                         .minimumScaleFactor(0.7)
-                        .frame(width: .infinity, height: 120)
 				}
 
 				VStack(spacing: 15) {
@@ -46,40 +46,21 @@ struct GameEndView: View {
 							gameModel.getPunishment()
 						} label: {
 							Text("Другое задание")
-								.font(.custom(CustomFonts.DelaGothicOne, size: 22))
-                                .frame(width: 274, height: 79)
 						}
-						.frame(width: 274, height: 79)
-						.background(.violet)
-						.foregroundColor(.yellowGradient)
-						.clipShape(RoundedRectangle(cornerRadius: 50))
-						.shadow(radius: 5, y: 5)
-						.overlay {
-							RoundedRectangle(cornerRadius: 50)
-								.stroke(.black)
-						}
+                        .customButtonEndViewStyle()
 					}
                     Button {
 						restartGame()
 					} label: {
 						Text("Начать заново")
-							.font(.custom(CustomFonts.DelaGothicOne, size: 22))
-                            .frame(width: 274, height: 79)
 					}
-					.frame(width: 274, height: 79)
-					.background(.violet)
-					.foregroundColor(.yellowGradient)
-					.clipShape(RoundedRectangle(cornerRadius: 50))
-					.shadow(radius: 5, y: 5)
-					.overlay {
-						RoundedRectangle(cornerRadius: 50)
-							.stroke(.black)
-					}
+                    .customButtonEndViewStyle()
 				}
+                .font(.custom(CustomFonts.DelaGothicOne, size: 22))
                 .padding(.bottom, 30)
+                .multilineTextAlignment(.center)
 			}
 		}
-	}
 }
 
 // MARK: - Methods
@@ -98,4 +79,27 @@ struct GameEndView_Previews: PreviewProvider {
 	static var previews: some View {
 		GameEndView(gameModel: GameViewModel())
 	}
+}
+
+// MARK: - Modifier
+
+struct CustomButtonEndViewStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 274, height: 79)
+            .background(.violet)
+            .foregroundColor(.yellowGradient)
+            .clipShape(RoundedRectangle(cornerRadius: 50))
+            .shadow(radius: 5, y: 5)
+            .overlay {
+                RoundedRectangle(cornerRadius: 50)
+                    .stroke(.black)
+            }
+    }
+}
+
+extension View {
+    func customButtonEndViewStyle() -> some View {
+        modifier(CustomButtonEndViewStyle())
+    }
 }
