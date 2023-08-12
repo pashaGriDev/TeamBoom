c0c56e56983c0fd709ef724d9c306c18d1a9e235
Artem <moiseev@physics.msu.ru>
2023-08-12 14:55:40 +0300

    EndGame_layout done

--- TeamBoom/Screens/GameView/GameEndView.swift
+++ TeamBoom/Screens/GameView/GameEndView.swift
@@ -8,94 +8,95 @@
 import SwiftUI
 
 struct GameEndView: View {
-	// MARK: - States&Properties
+    // MARK: - States&Properties
 
-	@ObservedObject var gameModel: GameViewModel
+    @ObservedObject var gameModel: GameViewModel
 
-	// MARK: - UI
+    // MARK: - UI
 
-	var body: some View {
-		ZStack {
-			BackgroundGradientView()
-			VStack {
-				if gameModel.withPunishment {
-					Text("Проигравший выполняет задание")
-						.padding()
-						.multilineTextAlignment(.center)
-						.font(.custom(CustomFonts.DelaGothicOne, size: 24))
-				} else {
-					Text("Время вышло!")
-						.padding()
-						.multilineTextAlignment(.center)
-						.font(.custom(CustomFonts.DelaGothicOne, size: 24))
-				}
-				Image("explosion")
-				if gameModel.withPunishment {
-					Text(gameModel.punishment)
+    var body: some View {
+        ZStack {
+            BackgroundGradientView()
+            VStack {
+                if gameModel.withPunishment {
+                    Text("Проигравший выполняет задание")
+                        .multilineTextAlignment(.center)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .font(.custom(CustomFonts.DelaGothicOne, size: 24))
+                } else {
+                    Text("Время вышло!")
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .font(.custom(CustomFonts.DelaGothicOne, size: 24))
+                }
+                Image("explosion")
+                    .resizable()
+                    .scaledToFit()
+                if gameModel.withPunishment {
+                    Text(gameModel.punishment)
                         .padding(8)
-						.foregroundColor(.violet)
-						.font(.custom(CustomFonts.DelaGothicOne, size: 20))
-						.multilineTextAlignment(.center)
+                        .foregroundColor(.violet)
+                        .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                        .multilineTextAlignment(.center)
                         .minimumScaleFactor(0.7)
-                        .frame(width: .infinity, height: 120)
-				}
+                       // .frame(width: .infinity, height: 120)
+                }
 
-				VStack(spacing: 15) {
-					if gameModel.withPunishment {
-						Button {
-							gameModel.getPunishment()
-						} label: {
-							Text("Другое задание")
-								.font(.custom(CustomFonts.DelaGothicOne, size: 22))
+                VStack(spacing: 15) {
+                    if gameModel.withPunishment {
+                        Button {
+                            gameModel.getPunishment()
+                        } label: {
+                            Text("Другое задание")
+                                .font(.custom(CustomFonts.DelaGothicOne, size: 22))
                                 .frame(width: 274, height: 79)
-						}
-						.frame(width: 274, height: 79)
-						.background(.violet)
-						.foregroundColor(.yellowGradient)
-						.clipShape(RoundedRectangle(cornerRadius: 50))
-						.shadow(radius: 5, y: 5)
-						.overlay {
-							RoundedRectangle(cornerRadius: 50)
-								.stroke(.black)
-						}
-					}
+                        }
+                        .frame(width: 274, height: 79)
+                        .background(.violet)
+                        .foregroundColor(.yellowGradient)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+                        .shadow(radius: 5, y: 5)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(.black)
+                        }
+                    }
                     Button {
-						restartGame()
-					} label: {
-						Text("Начать заново")
-							.font(.custom(CustomFonts.DelaGothicOne, size: 22))
-                            .frame(width: 274, height: 79)
-					}
-					.frame(width: 274, height: 79)
-					.background(.violet)
-					.foregroundColor(.yellowGradient)
-					.clipShape(RoundedRectangle(cornerRadius: 50))
-					.shadow(radius: 5, y: 5)
-					.overlay {
-						RoundedRectangle(cornerRadius: 50)
-							.stroke(.black)
-					}
-				}
-                .padding(.bottom, 30)
-			}
-		}
-	}
+                        restartGame()
+                    } label: {
+                        Text("Начать заново")
+                            .font(.custom(CustomFonts.DelaGothicOne, size: 22))
+                    }
+                    .frame(width: 274, height: 79)
+                    .background(.violet)
+                    .foregroundColor(.yellowGradient)
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
+                    .shadow(radius: 5, y: 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 50)
+                            .stroke(.black)
+                    }
+                }
+            }
+            .padding(.vertical, 8)
+        }
+    }
 }
 
 // MARK: - Methods
 
 extension GameEndView {
-	private func restartGame() {
-		gameModel.isPlaying = false
-		gameModel.isOver = false
+    private func restartGame() {
+        gameModel.isPlaying = false
+        gameModel.isOver = false
         gameModel.isPaused = false
-	}
+    }
 }
 
 // MARK: - Preview
 
 struct GameEndView_Previews: PreviewProvider {
-	static var previews: some View {
-		GameEndView(gameModel: GameViewModel())
-	}
+    static var previews: some View {
+        GameEndView(gameModel: GameViewModel())
+    }
 }
