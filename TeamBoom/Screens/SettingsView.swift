548bc448bca505d26ffe356b59ece5f790d11e83
Artem <moiseev@physics.msu.ru>
2023-08-12 03:04:27 +0300

    preLinting commit

--- TeamBoom/Screens/SettingsView.swift
+++ TeamBoom/Screens/SettingsView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct SettingsView: View {
     // MARK: - States&Properties
-
+    
     let columns = [GridItem(.adaptive(minimum: 155))]
     @StateObject  var gameModel: GameViewModel
-
-        // MARK: - Init
+    
+    // MARK: - Init
     init(gameModel: GameViewModel) {
         UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.tintColor
         let attributes: [NSAttributedString.Key: Any] = [
@@ -23,97 +23,95 @@ struct SettingsView: View {
         self._gameModel = StateObject(wrappedValue: gameModel)
     }
     // MARK: - UI
-
-	var body: some View {
-		NavigationView {
-			ZStack {
-				BackgroundGradientView()
-				VStack(alignment: .leading) {
-					Text("Время игры")
-						.font(.custom(CustomFonts.DelaGothicOne, size: 20))
-						.foregroundColor(Color.violet)
-						.shadow(radius: 5, y: 5)
-					LazyVGrid(columns: columns) {
-						ForEach(gameModel.getDurations().keys.sorted(), id: \.self) { duration in
-							Button(duration) {
-								changeDuration(duration: duration)
-							}
-							.font(.custom(CustomFonts.DelaGothicOne, size: 16))
-							.foregroundColor(gameModel.selectedDuration == duration ? .yellowOfButton : .violet)
-							.frame(width: 130)
-							.padding()
-							.background(gameModel.selectedDuration == duration ? .violet : .yellowOfButton)
-							.cornerRadius(30)
-							.opacity(1)
-							.overlay {
-								RoundedRectangle(cornerRadius: 30)
-									.stroke(.black)
-							}
-						}
-                        
-						.shadow(radius: 5, y: 5)
-					}
-                    .buttonStyle(ThemeAnimationStyle())
-					.padding(.horizontal)
-					Spacer()
-					VStack(spacing: 30) {
-						Toggle("Игра с заданиями", isOn: $gameModel.withPunishment)
-							.font(.custom(CustomFonts.DelaGothicOne, size: 20))
-							.foregroundColor(Color.violet)
-							.tint(Color.violet)
-						Toggle("Фоновая музыка", isOn: $gameModel.withBackgroundMusic)
-							.font(.custom(CustomFonts.DelaGothicOne, size: 20))
-							.foregroundColor(Color.violet)
-							.tint(Color.violet)
-                        VStack(alignment: .leading) {
-                            Text("Мелодия")
-                                .font(.custom(CustomFonts.DelaGothicOne, size: 20))
-                                .foregroundColor(Color.violet)
-                            Picker("", selection: $gameModel.backgroundSound) {
-                                ForEach(gameModel.backgroundSounds.keys.sorted(), id: \.self) { key in
-                                    Text(key)
-                                        .tag(gameModel.backgroundSounds[key] ?? "")
-                                }
-                            }
-                            .pickerStyle(.segmented)
-                            .background(.orangeGradient.opacity(0.1))
+    
+    var body: some View {
+        ZStack {
+            BackgroundGradientView()
+            VStack(alignment: .leading) {
+                Text("Время игры")
+                    .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                    .foregroundColor(Color.violet)
+                    .shadow(radius: 5, y: 5)
+                LazyVGrid(columns: columns) {
+                    ForEach(gameModel.getDurations().keys.sorted(), id: \.self) { duration in
+                        Button(duration) {
+                            changeDuration(duration: duration)
                         }
-
-                        VStack(alignment: .leading) {
-                            Text("Тиканье бомбы")
-                                .font(.custom(CustomFonts.DelaGothicOne, size: 20))
-                                .foregroundColor(Color.violet)
-                            Picker("", selection: $gameModel.tickSound) {
-                                ForEach(gameModel.tickSounds.keys.sorted(), id: \.self) { key in
-                                    Text(key)
-                                        .tag(gameModel.tickSounds[key] ?? "")
-                                }
-                            }
-                            .pickerStyle(.segmented)
-                            .background(.orangeGradient.opacity(0.1))
-                        }
-
-                        VStack(alignment: .leading) {
-                            Text("Взрыв бомбы")
-                                .font(.custom(CustomFonts.DelaGothicOne, size: 20))
-                                .foregroundColor(Color.violet)
-                            Picker("", selection: $gameModel.explosionSound) {
-                                ForEach(gameModel.bombSounds.keys.sorted(), id: \.self) { key in
-                                    Text(key)
-                                        .tag(gameModel.bombSounds[key] ?? "")
-                                }
-                            }
-                            .pickerStyle(.segmented)
-                            .background(.orangeGradient.opacity(0.1))
+                        .font(.custom(CustomFonts.DelaGothicOne, size: 16))
+                        .foregroundColor(gameModel.selectedDuration == duration ? .yellowOfButton : .violet)
+                        .frame(width: 130)
+                        .padding()
+                        .background(gameModel.selectedDuration == duration ? .violet : .yellowOfButton)
+                        .cornerRadius(30)
+                        .opacity(1)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(.black)
                         }
                     }
-                    Spacer()
-                    Spacer()
-                    Spacer()
+                    
+                    .shadow(radius: 5, y: 5)
                 }
+                .buttonStyle(ThemeAnimationStyle())
                 .padding(.horizontal)
-                .padding(.vertical, 20)
+                Spacer()
+                VStack(spacing: 30) {
+                    Toggle("Игра с заданиями", isOn: $gameModel.withPunishment)
+                        .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                        .foregroundColor(Color.violet)
+                        .tint(Color.violet)
+                    Toggle("Фоновая музыка", isOn: $gameModel.withBackgroundMusic)
+                        .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                        .foregroundColor(Color.violet)
+                        .tint(Color.violet)
+                    VStack(alignment: .leading) {
+                        Text("Мелодия")
+                            .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                            .foregroundColor(Color.violet)
+                        Picker("", selection: $gameModel.backgroundSound) {
+                            ForEach(gameModel.backgroundSounds.keys.sorted(), id: \.self) { key in
+                                Text(key)
+                                    .tag(gameModel.backgroundSounds[key] ?? "")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .background(.orangeGradient.opacity(0.1))
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Тиканье бомбы")
+                            .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                            .foregroundColor(Color.violet)
+                        Picker("", selection: $gameModel.tickSound) {
+                            ForEach(gameModel.tickSounds.keys.sorted(), id: \.self) { key in
+                                Text(key)
+                                    .tag(gameModel.tickSounds[key] ?? "")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .background(.orangeGradient.opacity(0.1))
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Взрыв бомбы")
+                            .font(.custom(CustomFonts.DelaGothicOne, size: 20))
+                            .foregroundColor(Color.violet)
+                        Picker("", selection: $gameModel.explosionSound) {
+                            ForEach(gameModel.bombSounds.keys.sorted(), id: \.self) { key in
+                                Text(key)
+                                    .tag(gameModel.bombSounds[key] ?? "")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .background(.orangeGradient.opacity(0.1))
+                    }
+                }
+                Spacer()
+                Spacer()
+                Spacer()
             }
+            .padding(.horizontal)
+            .padding(.vertical, 20)
         }
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
@@ -123,11 +121,13 @@ struct SettingsView: View {
                         .modifier(ToolBarButtonModifer())
                 }
             }
+        //.navigationBarBackButtonHidden(true)
+     //   .navigationBarItems(leading: CustomBackButton())
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: CustomBackButton())
     }
-}
+      
+    }
+
 
 // MARK: - Methods
 
